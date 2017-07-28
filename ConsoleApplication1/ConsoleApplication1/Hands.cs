b60d52444330c4d498d89327a641d5c5a350503b
Jasleen <Jasleen@LAPTOP-IREO6BKM>
2017-07-27 22:47:19 -0400

    Class named hands added

---
+++ ConsoleApplication1/ConsoleApplication1/Hands.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+/*
+ * Name: Jasleen
+ * Date: July 27, 2017
+ * Description:  Lesson 11
+ * Version: 0.3 -  Deck class
+ */
+namespace Cards
+{
+    public class Hand : CardList
+    {
+        // PRIVATE INSTANCE VARIABLES
+
+        // PUBLIC PROPERTIES
+
+        // CONSTRUCTORS
+
+        // PRIVATE METHODS
+        protected override void _initialize()
+        {
+            // STUB
+            // this method is empty
+        }
+
+        // PUBLIC METHODS
+
+        /// <summary>
+        /// This method overrides the built-in ToString method.
+        /// </summary>
+        /// <returns>
+        /// This method returns the current cards in the deck
+        /// </returns>
+        public override string ToString()
+        {
+            string outputString = "";
+
+            outputString += "Hand Contains  Number of Cards: " + this.Count + "\n";
+            outputString += "==================================\n";
+
+            foreach (Card card in this)
+            {
+                outputString += "The " + card.Face + " of " + card.Suit + "\n";
+            }
+
+            return outputString;
+        }
+        public void HighestCards(Hand hand)
+        {
+            var sortedHand = from cards in hand
+                             orderby cards.Face descending
+                             select cards.Face;
+            Console.WriteLine("The Highest cards in descending order: ");
+            foreach (var item in sortedHand)
+            {
+                {
+                    Console.WriteLine(item);
+                }
+
+
+            }
+        }
+    }
+}
---
+++ ConsoleApplication1/ConsoleApplication1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+/*
+ * Name: Jasleen
+ * Date: July 27, 2017
+ * Description: Lesson 11
+ * Version: 0.4  new Deck class
+ */
+
+namespace Cards
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Deck deck = new Deck();
+            Hand hand = new Hand();
+
+            Console.WriteLine(deck.ToString());
+            Console.WriteLine();
+            deck.Shuffle();
+            Console.WriteLine("Shuffled");
+            Console.WriteLine(deck.ToString());
+
+            hand.Add(deck.Deal1());
+            Console.WriteLine(hand.ToString());
+            Console.WriteLine();
+            hand.Add(deck.Deal1());
+            Console.WriteLine(hand.ToString());
+
+            Console.WriteLine();
+            Console.WriteLine(deck.ToString());
+
+            //hand = deck.Deal5(); // moves the top 5 cards from the deck to the hand object
+
+            deck.Shuffle();
+            Console.WriteLine("Shuffled");
+            Console.WriteLine(deck.ToString());
+            Console.WriteLine("Dealt Top Five Cards");
+            Console.WriteLine("==================================");
+            Console.WriteLine();
+
+            //Console.WriteLine(deck.ToString());
+            hand = deck.Deal5(); //moves top five cards from the deck to the  
+            //Console.WriteLine(hand);
+            Console.WriteLine(hand.ToString());
+            hand.HighestCards(hand);
+            // Console.WriteLine(hand.HighestCards());
+
+        }
+    }
+}
